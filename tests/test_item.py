95a6597c6c33bdf7bd588e27ab973394a50b84e3
vasya_k <vasya_kukarsev@mail.ru>
2024-01-13 17:08:37 +0300

    Созданы тесты для класса и сам класс

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+def test_price():
+    item = Item('Test_name', price=100, quantity=5)
+    assert item.calculate_total_price() == 500
+
+def test_apply_discount():
+    item = Item('Test_name', price=100, quantity=3)
+    Item.pay_rate = 0.7
+    item.apply_discount()
+    assert item.price == 70
